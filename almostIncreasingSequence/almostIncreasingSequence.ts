df9dc51e862757f7402eeb0fb4b6dbbec2cd31bf
Abishek Bardewa <49955388+abishekbardewa@users.noreply.github.com>
2020-12-13 18:55:21 +0530

    My Solution

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++
@@ -1,6 +0,0 @@
-function almostIncreasingSequence(sequence: number[]): number {
-	return 4;
-}
-
-// console.log(almostIncreasingSequence([1, 3, 2, 1]))
-// console.log(almostIncreasingSequence([1, 3, 2]))
